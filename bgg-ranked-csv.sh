9182432eea36b017c2fb18eea1b9d22fc3fe0306
Markus Schepke <markus.schepke@wolt.com>
2023-02-21 21:18:21 +0200

    remove all things Git and added compose.yaml

--- bgg-ranked-csv.sh
+++ bgg-ranked-csv.sh
@@ -2,30 +2,10 @@
 
 set -e
 
-OUTPUT_FILE=$(date --iso-8601).csv
-# We embed the source under /data in case we want to mount in
-SOURCE_DIR=/data/bgg-ranking-historicals
+OUTPUT_FILE="$(date --utc '+%Y-%m-%dT%H-%M-%S').csv"
+SOURCE_DIR='/data/bgg-ranking-historicals'
 
-# @see https://git-scm.com/docs/gitfaq#http-credentials-environment
-git config --global credential.helper \
-	'!f() { echo username=$GIT_CREDENTIAL_USERNAME; echo "password=$GIT_CREDENTIAL_PASSWORD"; };f'
-git config --global user.name "$GIT_USER_NAME"
-git config --global user.email "$GIT_USER_EMAIL"
+mkdir -p "${SOURCE_DIR}"
 
-mkdir -p /data
-
-if [ -d "$SOURCE_DIR" ]; then
-  # We mounted the repo in, so just clean and pull
-  cd "$SOURCE_DIR"
-  git stash -u
-  git pull --no-rebase origin master
-else
-  # No repo yet, clone it
-  git clone --depth=1 https://github.com/beefsack/bgg-ranking-historicals.git "$SOURCE_DIR"
-fi
-  
-cd "$SOURCE_DIR"
-bgg-ranked-csv > "$OUTPUT_FILE"
-git add "$OUTPUT_FILE"
-git commit -m "Added $OUTPUT_FILE"
-git push origin master
+cd "${SOURCE_DIR}"
+bgg-ranked-csv > "${OUTPUT_FILE}"
