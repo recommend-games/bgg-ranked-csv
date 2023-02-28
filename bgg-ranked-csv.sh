45eb85253b6f2ae137c7b0555c4643576f77c566
Markus Schepke <markus.schepke@wolt.com>
2023-02-28 09:40:54 +0200

    Added git commit and push to bgg-ranked-csv.sh

--- bgg-ranked-csv.sh
+++ bgg-ranked-csv.sh
@@ -10,9 +10,22 @@ OUTPUT_FILE="$(date --utc '+%Y-%m-%dT%H-%M-%S').csv"
 
 mkdir --parents "${DATA_DIR}"
 
-cd "${SOURCE_DIR}"
 echo "Fetch BGG rankings and write results to <${DATA_DIR}/${OUTPUT_FILE}>"
+cd "${SOURCE_DIR}"
 ./bgg-ranked-csv > "${DATA_DIR}/${OUTPUT_FILE}"
 echo "Done fetching."
+
+echo "Commit results to repo <${DATA_DIR}>"
+cd "${DATA_DIR}"
+git add "${DATA_DIR}/${OUTPUT_FILE}"
+git commit --no-gpg-sign \
+    --message "Added ${OUTPUT_FILE}"
+
+echo "Push new commits to remote repos"
+for REMOTE in $(git remote); do
+    echo "Pushing to <${REMOTE}>"
+    git push "${REMOTE}" || echo "Unable to push to <${REMOTE}>…"
+done
+echo "Done."
 
 cd "${SAVE_DIR}"
